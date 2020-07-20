c7f0c97cb538ee7f4b94c772f955ff6ef7dcbe84
Arun Karthick S <68554357+ARUNKARTHICK-S@users.noreply.github.com>
2020-07-20 21:04:17 +0530

    Create birthday cake candles.py

---
+++ birthday cake candles.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+
+import os
+
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    maximum=max(ar)
+    count=0
+    for i in range(len(ar)):
+        if ar[i]==maximum:
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
